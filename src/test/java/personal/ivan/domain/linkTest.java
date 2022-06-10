d88a14c08ef2a063259cbd7876bda6f625288247
Харитонин Иван <i.haritonin@crpt.ru>
2022-06-10 18:45:46 +0300

    link

--- src/test/java/personal/ivan/domain/linkTest.java
+++ src/test/java/personal/ivan/domain/linkTest.java
@@ -1,4 +1,18 @@
 package personal.ivan.domain;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import personal.ivan.parse.AsciidocParser;
+
 public class linkTest {
+    @Test
+    void test() {
+        AsciidocParser parser = new AsciidocParser();
+        Document d = parser.parseAsciiDocument(ClassLoader.getSystemResource("linkTest.adoc").getPath());
+        for (var c : d.children) {
+            if (c instanceof Link) {
+                Assertions.assertEquals("https://честныйзнак.рф/", c.content);
+            }
+        }
+    }
 }
