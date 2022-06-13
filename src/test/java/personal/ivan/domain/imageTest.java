10b2dc447910fe6edd4310aa65860d17681847a6
Харитонин Иван <i.haritonin@crpt.ru>
2022-06-13 23:57:50 +0300

    image test

--- src/test/java/personal/ivan/domain/imageTest.java
+++ src/test/java/personal/ivan/domain/imageTest.java
@@ -1,11 +1,28 @@
 package personal.ivan.domain;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import personal.ivan.GlobalConstants;
+import personal.ivan.parse.AsciidocParser;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class imageTest {
-    @Test void test()
-    {
-
+    @Test
+    void test() {
+        AsciidocParser parser = new AsciidocParser();
+        for (var obj : parser.parseAsciiDocument(ClassLoader.getSystemResource("imageTest.adoc").getPath()).children
+        ) {
+            if (obj instanceof Image) {
+                Pattern pat = Pattern.compile(GlobalConstants.pngRegexp);
+                String s = ((Image) obj).path.getFileName().toString();
+                Matcher mat = pat.matcher(s);
+                //нашлось ли хоть одно совпадение
+                Assertions.assertTrue(mat.find());
+                //совпадение равно длине всей строки или нет
+                Assertions.assertTrue(mat.group().length() == s.length());
+            }
+        }
     }
-
 }
