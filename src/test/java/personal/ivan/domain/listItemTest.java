39da6f1556f2b46b2a22718448294fb60376764c
Харитонин Иван <i.haritonin@crpt.ru>
2022-06-17 10:56:44 +0300

    listItem_branch

---
+++ src/test/java/personal/ivan/domain/listItemTest.java
@@ -0,0 +1,18 @@
+package personal.ivan.domain;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import personal.ivan.parse.AsciidocParser;
+
+public class listItemTest {
+    @Test void test()
+    {
+        AsciidocParser parser = new AsciidocParser();
+        for (var obj : parser.parseAsciiDocument(ClassLoader.getSystemResource("listItemTest.adoc").getPath()).children
+        ) {
+            if (obj instanceof ListItem) {
+                Assertions.assertEquals("направить обращение в СТП;",obj.content);
+            }
+        }
+    }
+}
