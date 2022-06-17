9d2a3707568cf94cefea85872680e5c0f570da16
Харитонин Иван <i.haritonin@crpt.ru>
2022-06-17 15:10:50 +0300

    test

--- src/main/java/personal/ivan/domain/Cell.java
+++ src/main/java/personal/ivan/domain/Cell.java
@@ -1,6 +1,8 @@
 package personal.ivan.domain;
 
-public class Cell extends Element{
+public class Cell extends Element {
+    public int a = 5;
+    public String s ="a";
     public Cell(String text) {
         super(text);
     }
