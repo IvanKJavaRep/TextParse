4a6c4461b4475e10d94fbe27764d1f4deb2d0fa5
Харитонин Иван <i.haritonin@crpt.ru>
2022-06-27 23:44:13 +0300

    parameterized test for document object added

--- src/test/java/personal/ivan/domain/lombok/documentBuilderTest.java
+++ src/test/java/personal/ivan/domain/lombok/documentBuilderTest.java
@@ -1,14 +1,18 @@
 package personal.ivan.domain.lombok;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import personal.ivan.domain.Document;
 
 public class documentBuilderTest {
-    @Test
-    void test() {
-        Document doc = Document.builder().text("Document").build();
-        Assertions.assertEquals("Document(super=Element(content=Document, children=[], properties={}), elements=[])", doc.toString());
+
+    @ParameterizedTest
+    @ValueSource(strings = {"D", "Doc", "Document"})
+    void test(String arg) {
+        Document doc = Document.builder().text(arg).build();
+        Assertions.assertEquals(String.format("Document(super=Element(content=%s, children=[], properties={}), elements=[])", arg), doc.toString());
+
 
     }
 }
