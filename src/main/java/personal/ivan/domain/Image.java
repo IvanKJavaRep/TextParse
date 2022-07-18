73381bcc33b5d32fc71070db20611e84150b382d
Харитонин Иван <i.haritonin@crpt.ru>
2022-07-18 14:55:56 +0300

    memento

--- src/main/java/personal/ivan/domain/Image.java
+++ src/main/java/personal/ivan/domain/Image.java
@@ -8,17 +8,30 @@ import java.util.Map;
 
 @Getter
 @Setter
-public class Image extends Element{
+public class Image extends Element {
     private Map<String, Object> map;
     private Path path;
+
     public Image(String text, Path p, Map<String, Object> m) {
         super(text);
-        path=p;
-        map=m;
+        path = p;
+        map = m;
     }
 
     @Override
     public void printContent() {
+    }
+
+    public void SetMemento(Memento memento) {
+        path = memento.getPath();
+        map = memento.getMap();
 
     }
+
+    public Memento GetMemento() {
+        return new Memento(this.path, this.map);
+    }
+
+
 }
+
