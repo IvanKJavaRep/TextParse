bdb13c39fbfd69bae87cb6a5fdddba0d93145294
Харитонин Иван <i.haritonin@crpt.ru>
2022-09-08 11:58:41 +0300

    property resourse file done

--- src/test/java/personal/ivan/domain/ParagraphTest.java
+++ src/test/java/personal/ivan/domain/ParagraphTest.java
@@ -9,14 +9,14 @@ public class ParagraphTest {
     void test() {
         AsciidocParser parser = new AsciidocParser();
         Document d = parser.parse(ClassLoader.getSystemResource("paragraphTest.adoc").getPath());
-        Assertions.assertEquals("<strong>Пример строки запроса:</strong>", d.getChildren().get(0).getContent());
+        Assertions.assertEquals("<strong>РџСЂРёРјРµСЂ СЃС‚СЂРѕРєРё Р·Р°РїСЂРѕСЃР°:</strong>", d.getChildren().get(0).getContent());
     }
 
     @Test
     void test1() {
         AsciidocParser parser = new AsciidocParser();
         Document d = parser.parse(ClassLoader.getSystemResource("paragraphTest1.adoc").getPath());
-        Assertions.assertEquals("Пример строки запроса:", d.getChildren().get(0).getContent());
+        Assertions.assertEquals("РџСЂРёРјРµСЂ СЃС‚СЂРѕРєРё Р·Р°РїСЂРѕСЃР°:", d.getChildren().get(0).getContent());
 
     }
 
