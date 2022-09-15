967fae89b17b8c6579ce536bc7d94045bb6b7424
Харитонин Иван <i.haritonin@crpt.ru>
2022-09-15 13:16:03 +0300

    @Lookup added and question

--- src/main/java/personal/ivan/spring/dependsOn/Car.java
+++ src/main/java/personal/ivan/spring/dependsOn/Car.java
@@ -1,8 +1,23 @@
 package personal.ivan.spring.dependsOn;
 
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope("prototype")
 public class Car {
     Engine engine;
     Wheels wheels;
+    String passengerName;
+
+    public String getPassengerName() {
+        return passengerName;
+    }
+
+    public void setPassengerName(String passengerName) {
+        this.passengerName = passengerName;
+    }
+
 
     public Car(Engine e, Wheels wheels) {
         System.out.println("-- Car Initialized --");
